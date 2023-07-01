a50735e31318920497a71fe9b2153e2227db2370
Mustaneer Haider <66249988+mustaneerhaider01@users.noreply.github.com>
2023-07-01 15:54:43 +0500

    cors optiosn modified

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.use(cors());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 8000);
 }
