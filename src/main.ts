8e68bfe8f779a1a110a2f4f7770ae541fac7d932
Mustaneer Haider <66249988+mustaneerhaider01@users.noreply.github.com>
2023-07-01 15:34:40 +0500

    cors added

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 8000);
