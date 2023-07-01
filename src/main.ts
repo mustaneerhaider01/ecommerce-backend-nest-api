cf8351f9d9d7e47716424ebadab8735dad283400
Mustaneer Haider <66249988+mustaneerhaider01@users.noreply.github.com>
2023-07-01 16:00:30 +0500

    cors not working :(

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 8000);
 }
